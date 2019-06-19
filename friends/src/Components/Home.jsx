2bf797176c795665f29d4cf8e35b517924a2b49f
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-19 16:06:33 +0100

    Home component created for Homepage

---
+++ friends/src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home(props) {
+  return (
+   <form>
+      Name: 
+      <input type='text' placeholder='name'/>
+      Age: 
+      <input type='number' placeholder='age'/>
+      Email:
+      <input type='email' placeholder='e.g example@example.com'/>
+   </form>
+  );
+}
+
+export default Home;
